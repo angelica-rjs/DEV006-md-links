58f44c90c2c942897669cbbe96605b74b1716e59
angelica-rjs <angelicarojas.rys@gmail.com>
2023-06-13 18:03:46 -0400

    modificada funcion validate, funcion para verificar si la ruta existe terminada

--- index.js
+++ index.js
@@ -6,52 +6,53 @@
 const path = require('path');
 let fs = require('fs');
 
-function prueba(){
-let ruta = '/hola.txt'
-mdLink(ruta);
+
+mdLink('C://Users/ange_/DEV006-md-links/hola.txt')
+
+function mdLink(ruta, option = {validate: false}){
+  validate(ruta, option)
+  ruta = generateRoute(ruta)
+  console.log('Ruta absoluta:', ruta);
+  routeExists(ruta)
+  //TODO: Verificar si es directorio o archivo
+  esRutaoArchivo(ruta)
+
+  //TODO: recore (procesa)la ruta 
+
+  //TODO: crear una promesa y a regresar
 }
 
-function mdLink(ruta){
+function validate(ruta){
+  if(ruta == null){
+    throw new TypeError("La ruta no debe ser nula")
+  }
+}
+
+function generateRoute(ruta){
   if (path.isAbsolute(ruta)){
     console.log("es absoluta")
   }else{
-    route(ruta)
-  }
- 
-}
-prueba();
-
-
-/*function validate(ruta, option){
-
-const ruta = "./hola.txt";
-  const option = true;
-  if( ruta  !== null && typeof option === "boolean"){
-    if (path.isAbsolute(ruta)){
-
-    }else{
-      route(ruta)
+    ruta = path.resolve(ruta);
     }
-  }else{
-    TypeError("debe recibir dos argumento")
-  }
-  
-}*/
-
-
-
-
-
-function route(ruta){
-  //"./hola.txt"
-  const rutaRelativa = ruta;
-  const rutaAbsoluta = path.resolve(rutaRelativa);
-  console.log('Ruta absoluta:', rutaAbsoluta);
+  return ruta
 }
-//route()
+
+function routeExists(ruta){
+  try {
+    fs.accessSync(ruta);
+    console.log("la ruta existe")
+    return ruta
+  } catch (error) {
+    throw new TypeError("La ruta no existe")
+  }
+}
 
 
+/*function esRutaoArchivo(ruta){
+  if(fs.lstatSync(ruta).isDirectory()){
+    console.log("es un directorio")
+  }else{
+    console.log("es un archivo")
+  }
 
-
-
-
+}*/
