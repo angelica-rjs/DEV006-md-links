69b98775f08e9f479e8baa8ee77244c4dc010f36
angelica-rjs <angelicarojas.rys@gmail.com>
2023-06-27 23:05:32 -0400

    flugo funcionando

--- index.js
+++ index.js
@@ -14,68 +14,47 @@ const axios = require('axios');
 
 let allFiles = []
 
-mdLink('C://Users/ange_/DEV006-md-links/prueban/pruebam/pruebam.md', {validate: true})
+mdLink('prueban/pruebam/pruebam.md', {validate: true})
 /*-------------------------------------------------------- */
-function mdLink(ruta, option = { validate: false }) {
-  return new Promise((resolve, reject) => {
-    validateParameter(ruta, option);
-    ruta = generateRoute(ruta);
-    routeExists(ruta);
-    let pathType = isDirectory(ruta);
-    let filesMd;
-    if (pathType == true) {
-      const allFile = searchFiles(ruta);
-      filesMd = getMds(allFile);
-      const promises = filesMd.map((file) => processFile(file, option));
-      Promise.all(promises)
-        .then((results) => {
-          const flattenedResults = results.flat();
-          flattenedResults.forEach((result) => {
-            console.log(result);
-          });
-          resolve(flattenedResults);
-        })
-        .catch((error) => {
-          console.error(error);
-          reject(error);
-        });
-    } else {
-      allFiles.push(ruta);
-      filesMd = getMds(allFiles);
-      processFile(filesMd[0], option)
-        .then((result) => {
-          console.log(result[0]);
-          resolve(result);
-        })
-        .catch((error) => {
-          console.error(error);
-          reject(error);
-        });
+function mdLink(ruta, option = {validate: false}){
+  return new Promise((resolve, reject) =>{
+    validateParameter(ruta, option)
+    ruta = generateRoute(ruta)
+    routeExists(ruta)
+    let pathType= isDirectory(ruta)
+    let filesMd
+    if( pathType == true){
+    const allFile = searchFiles(ruta)
+    filesMd=getMds(allFile)
+    console.log(filesMd, "archivos que se enviaran a process file")
+    filesMd.forEach((file) => {
+    processFile(file)
+    });
+    }else{
+    allFiles.push(ruta)
+    filesMd= getMds(allFiles)
+    console.log(filesMd, "archivos que se enviaran a process file")
+    const fileMd = filesMd[0];
+    processFile(fileMd)
     }
-  });
+  })
 }
 
-function processFile(file, option) {
+
+/*-------------------------------------------------------- */
+function processFile(file){
   console.log(file);
-  return new Promise((resolve, reject) => {
-    readFile(file)
-      .then((data) => {
-        let objLink = getLinks(data, file);
-        if (option.validate === true) {
-          return getEstatus(objLink);
-        } else {
-          return objLink;
-        }
-      })
-      .then((result) => {
-       // console.log(result);
-        resolve(result);
-      })
-      .catch((error) => {
-        console.error(error);
-        reject(error);
-      });
-  });
+  readFile(file)
+    .then((data) => {
+
+     const links=  getLinks(data,file)
+     console.log("los links encontrado: ",links)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+
 }
 
 //TERMINADAS
@@ -91,7 +70,8 @@ function validateParameter(ruta, option) {
 
 function generateRoute(ruta){
   if(!path.isAbsolute(ruta)){
-    return path.resolve(ruta);
+    const pathObsolute =  path.resolve(ruta);
+    return pathObsolute
   }else{
   return ruta
   }
